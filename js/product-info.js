a21852472461e1d0e80b1dbf83727ba462de79fa
andreafernandez28 <110433567+andreafernandez28@users.noreply.github.com>
2022-09-19 23:34:31 -0300

    E4: desafiate añadido a las fotos

--- js/product-info.js
+++ js/product-info.js
@@ -45,20 +45,37 @@ function showProductsInfo () {
                 </div>
             </div>   
         </div>  
-        <div class="row">
 
-            <div class="column">
-                <img src="` + productsInfoArray.images[0] + `" alt="img1" style="width:100%">
+  
+        <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
-            <div class="column">
-                <img src="` + productsInfoArray.images[1] + `" alt="img2" style="width:100%">
+            <div class="carousel-inner">
+                <div class="carousel-item active" data-bs-interval="10000">
+                    <img src="` + productsInfoArray.images[0] + `" class="d-block w-50" alt="img1">
+                </div>
+                <div class="carousel-item " data-bs-interval="2000">
+                    <img src="` + productsInfoArray.images[1] + `"  class="d-block w-50" alt="img2" >
+                </div>
+                <div class="carousel-item ">
+                    <img src="` + productsInfoArray.images[2] + `"  class="d-block w-50" alt="img3">
+                </div>
+                <div class="carousel-item ">
+                    <img src="` + productsInfoArray.images[3] + `"  class="d-block w-50" alt="img4">
+                </div>
             </div>
-            <div class="column">
-                <img src="` + productsInfoArray.images[2] + `" alt="img3" style="width:100%">
-            </div>
-            <div class="column">
-                <img src="` + productsInfoArray.images[3] + `" alt="img4" style="width:100%">
-            </div>
+
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+           <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+           </button>
         </div>
      `
     document.getElementById('products-info').innerHTML += htmlContentToAppend;
